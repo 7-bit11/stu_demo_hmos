76f9f32152b4e1f0f308b22f70ebc9d750040a26
zhangyu <210014468@qq.com>
2024-07-29 17:33:59 +0800

    ✨ feat(Preferences 工具类): 本地存储数据工具类 数据量不能太大 可以存储用户本地设置类型的值

--- entry/src/main/ets/component/PreferencesUtil.ts
+++ entry/src/main/ets/component/PreferencesUtil.ts
@@ -7,7 +7,7 @@ class PreferencesUtil {
   async init(context, name?: string) {
     try {
       // 加载初始化Preferences
-      let data = await preferences.getPreferences(context, name)
+      let data = await preferences.getPreferences(context, name?? this.defaultName)
       this.pref.set(name ?? this.defaultName, data)
       console.log("PreferencesUtil.init.SUCCESS")
     } catch (e) {
@@ -22,24 +22,24 @@ class PreferencesUtil {
     }
     try {
       let data = this.pref.get(name ?? this.defaultName);
-      let preValue = await data?.get(key, defValue)
-      console.log("PreferencesUtil.getValue.SUCCESS", preValue)
+      let preValue = await data.get(key, defValue)
+      console.log("PreferencesUtil.getValue.SUCCESS", `成功获取${key}=${preValue}]`)
       return preValue;
     } catch (e) {
       console.log("PreferencesUtil.getValue.ERROR", e)
     }
   }
 
-  async putValue(key: string, defValue: preferences.ValueType, name?: string) {
+  async putValue(key: string, value: preferences.ValueType, name?: string) {
     if (!this.pref.has(name ?? this.defaultName)) {
       console.log("PreferencesUtil.getValue.IsNotKey", "没有找到初始化的Preferences")
       return
     }
     try {
       let data = this.pref.get(name ?? this.defaultName);
-      let preValue = await data?.put(key, defValue)
-      console.log("PreferencesUtil.getValue.SUCCESS", preValue)
-      return preValue;
+      await data.put(key, value)
+      await data?.flush();
+      console.log("PreferencesUtil.putValue.SUCCESS", `成功写入${key}-${value}=[${value}]`)
     } catch (e) {
       console.log("PreferencesUtil.getValue.ERROR", e)
     }
